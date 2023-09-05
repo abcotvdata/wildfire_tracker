8fef85590cb4bbc57a57f506e591b32bb35c675e
John Kelly <jkelly3rd@gmail.com>
2023-09-05 10:21:06 -0400

    Update get_satellite_data.R

--- scripts/get_satellite_data.R
+++ scripts/get_satellite_data.R
@@ -40,7 +40,7 @@ try(file.rename(paste(sep="",
 hotspot_url2 <- paste(sep="","https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Fire_Points/Text/",
                       format(Sys.Date(), "%Y"),"/",
                       format(Sys.Date(), "%m"),"/",
-                      "hms_fire",format(Sys.Date()-1, "%Y%m%d"),".txt")
+                      "hms_fire",format(Sys.Date(), "%Y%m%d"),".txt")
 hotspot_url1 <- paste(sep="","https://satepsanone.nesdis.noaa.gov/pub/FIRE/web/HMS/Fire_Points/Text/",
                       format(Sys.Date(), "%Y"),"/",
                       format(Sys.Date(), "%m"),"/",
