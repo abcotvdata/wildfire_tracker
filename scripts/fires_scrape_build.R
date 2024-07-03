8168623357edf77635c6bd6132153aee8b366ebf
lindseyf96 <39309451+lindseyf96@users.noreply.github.com>
2024-07-03 11:52:41 -0700

    Update fires_scrape_build.R

--- scripts/fires_scrape_build.R
+++ scripts/fires_scrape_build.R
@@ -327,7 +327,7 @@ fireIcons <- awesomeIcons(
   iconColor = "white",
   library = 'glyphicon',
   squareMarker = TRUE,
-  markerColor = ifelse(fires$percent_contained == 100, "gray", "orange")))
+  markerColor = ifelse(fires$percent_contained == 100, "gray", "orange"))
 # options include ion-flame, ion-fireball, fa-fire
 
 # Set values for EasyButtonBar controls here
